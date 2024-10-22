8c952d018077c8da85b61e7c8f17b9e253ead5b3
Craig Hathaway <github@saropa.com>
2024-10-22 10:02:14 -0400

    Add CommonRandom class

---
+++ lib/random/common_random.dart
@@ -0,0 +1,17 @@
+import 'dart:math';
+
+/// Every time you reinstall your app, the CommonRandom() is being initialized
+///  with the same starting conditions. To ensure you get a different result
+///  each time, use the current timestamp as a seed. This makes the seed
+///  dynamic and unique each time.
+///
+/// NOTE: if you call CommonRandom() multiple times in quick succession, they
+///  might generate the same value due to the seed being based on
+///  [DateTime.now().millisecondsSinceEpoch]
+///
+/// To get distinct values, you should initialize CommonRandom once and reuse
+///  it.
+///
+// ignore: non_constant_identifier_names
+Random CommonRandom([int? seed]) =>
+    Random(seed ?? DateTime.now().millisecondsSinceEpoch);
