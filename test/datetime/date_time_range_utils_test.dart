e375677aa7251689739670e75ca2d8adb7a30d1f
Craig Hathaway <github@saropa.com>
2025-01-07 22:00:29 -0500

    more date range utils

--- test/datetime/date_time_range_utils_test.dart
+++ test/datetime/date_time_range_utils_test.dart
@@ -3,6 +3,211 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:saropa_dart_utils/datetime/date_time_range_utils.dart';
 
 void main() {
+  group('isNthDayOfMonthInRange', () {
+    test('returns true for 2nd Monday of March within range', () {
+      final range = DateTimeRange(
+        start: DateTime(2023),
+        end: DateTime(2023, 12, 31),
+      );
+      expect(range.isNthDayOfMonth(2, DateTime.monday, 3), true);
+    });
+
+    test('returns false for 2nd Monday of March outside range', () {
+      final range = DateTimeRange(
+        start: DateTime(2023, 4),
+        end: DateTime(2023, 12, 31),
+      );
+      expect(range.isNthDayOfMonth(2, DateTime.monday, 3), false);
+    });
+
+    test('returns true for 1st Friday of January at start of range', () {
+      final range = DateTimeRange(
+        start: DateTime(2023),
+        end: DateTime(2023, 12, 31),
+      );
+      expect(range.isNthDayOfMonth(1, DateTime.friday, 1), true);
+    });
+
+    test('returns true for last day of range', () {
+      final range = DateTimeRange(
+        start: DateTime(2023),
+        end: DateTime(2023, 12, 31),
+      );
+      expect(range.isNthDayOfMonth(5, DateTime.sunday, 12), true);
+    });
+
+    test('returns false for day just after range', () {
+      final range = DateTimeRange(
+        start: DateTime(2023),
+        end: DateTime(2023, 12, 30),
+      );
+      expect(range.isNthDayOfMonth(5, DateTime.sunday, 12), false);
+    });
+
+    test('returns true for recurring holiday across multiple years', () {
+      final range = DateTimeRange(
+        start: DateTime(2022),
+        end: DateTime(2024, 12, 31),
+      );
+      expect(range.isNthDayOfMonth(3, DateTime.monday, 1), true);
+    });
+
+    test('returns false when nth occurrence does not exist', () {
+      final range = DateTimeRange(
+        start: DateTime(2023),
+        end: DateTime(2023, 12, 31),
+      );
+      expect(
+        range.isNthDayOfMonth(5, DateTime.monday, 2),
+        false,
+      ); // February never has 5 Mondays
+    });
+
+    test('returns true for leap year day when in range', () {
+      final range = DateTimeRange(
+        start: DateTime(2024),
+        end: DateTime(2024, 12, 31),
+      );
+      expect(
+        range.isNthDayOfMonth(5, DateTime.thursday, 2),
+        true,
+      ); // February 29, 2024
+    });
+
+    test('returns false for leap year day when not in range', () {
+      final range = DateTimeRange(
+        start: DateTime(2023),
+        end: DateTime(2023, 12, 31),
+      );
+      expect(
+        range.isNthDayOfMonth(5, DateTime.thursday, 2),
+        false,
+      ); // February 29, 2024 not in 2023
+    });
+
+    test('returns true for 1st Monday of January in multi-year range', () {
+      final range =
+          DateTimeRange(start: DateTime(2023), end: DateTime(2025, 12, 31));
+      expect(range.isNthDayOfMonth(1, DateTime.monday, 1), true);
+    });
+
+    test('returns false for 6th Friday of April (impossible case)', () {
+      final range =
+          DateTimeRange(start: DateTime(2023), end: DateTime(2023, 12, 31));
+      expect(range.isNthDayOfMonth(6, DateTime.friday, 4), false);
+    });
+
+    test('returns true for 5th Wednesday of May 2023 (May 31, 2023)', () {
+      final range =
+          DateTimeRange(start: DateTime(2023), end: DateTime(2023, 12, 31));
+      expect(range.isNthDayOfMonth(5, DateTime.wednesday, 5), true);
+    });
+
+    test('returns false for 5th Wednesday of May 2024', () {
+      final range =
+          DateTimeRange(start: DateTime(2024), end: DateTime(2024, 12, 31));
+      expect(range.isNthDayOfMonth(5, DateTime.wednesday, 5), true);
+    });
+
+    test('returns true for 4th Thursday of November (Thanksgiving) in 2023',
+        () {
+      final range =
+          DateTimeRange(start: DateTime(2023), end: DateTime(2023, 12, 31));
+      expect(range.isNthDayOfMonth(4, DateTime.thursday, 11), true);
+    });
+
+    test(
+        'returns false for 4th Thursday of November (Thanksgiving) in range '
+        'ending before November', () {
+      final range =
+          DateTimeRange(start: DateTime(2023), end: DateTime(2023, 10, 31));
+      expect(range.isNthDayOfMonth(4, DateTime.thursday, 11), false);
+    });
+
+    test('returns true for 1st Sunday of February in leap year 2024', () {
+      final range =
+          DateTimeRange(start: DateTime(2024), end: DateTime(2024, 12, 31));
+      expect(range.isNthDayOfMonth(1, DateTime.sunday, 2), true);
+    });
+
+    test('returns false for 5th Saturday of December 2023', () {
+      final range =
+          DateTimeRange(start: DateTime(2023), end: DateTime(2023, 12, 31));
+      expect(range.isNthDayOfMonth(5, DateTime.saturday, 12), true);
+    });
+
+    test(
+        'returns true for 2nd Tuesday of February in non-leap year range '
+        'including multiple years', () {
+      final range =
+          DateTimeRange(start: DateTime(2023), end: DateTime(2025, 12, 31));
+      expect(range.isNthDayOfMonth(2, DateTime.tuesday, 2), true);
+    });
+
+    test('returns false when nth occurrence does not exist', () {
+      final range = DateTimeRange(
+        start: DateTime(2023),
+        end: DateTime(2023, 12, 31),
+      );
+      expect(
+        range.isNthDayOfMonth(5, DateTime.monday, 2),
+        false,
+      ); // February never has 5 Mondays
+    });
+
+    test('returns false for 6th Friday of April (impossible case)', () {
+      final range =
+          DateTimeRange(start: DateTime(2023), end: DateTime(2023, 12, 31));
+      expect(range.isNthDayOfMonth(6, DateTime.friday, 4), false);
+    });
+
+    test('returns false for 5th Wednesday of May 2024', () {
+      final range =
+          DateTimeRange(start: DateTime(2024), end: DateTime(2024, 12, 31));
+      expect(range.isNthDayOfMonth(5, DateTime.wednesday, 5), true);
+    });
+
+    test('returns true for 5th Saturday of December 2024 (does not exist)', () {
+      final range =
+          DateTimeRange(start: DateTime(2024), end: DateTime(2024, 12, 31));
+      expect(range.isNthDayOfMonth(5, DateTime.saturday, 12), false);
+    });
+
+    test('returns true for 1st Monday of January in multi-year range', () {
+      final range =
+          DateTimeRange(start: DateTime(2023), end: DateTime(2025, 12, 31));
+      expect(range.isNthDayOfMonth(1, DateTime.monday, 1), true);
+    });
+
+    test('returns true for February 29 in leap year', () {
+      final range =
+          DateTimeRange(start: DateTime(2024), end: DateTime(2024, 12, 31));
+      expect(
+        range.isNthDayOfMonth(1, DateTime.thursday, 2),
+        true,
+      ); // February has a Thursday on Feb.29
+    });
+
+    test('returns false for February when asking for a day that does not exist',
+        () {
+      final range =
+          DateTimeRange(start: DateTime(2023), end: DateTime(2023, 12, 31));
+      expect(
+        range.isNthDayOfMonth(5, DateTime.monday, 2),
+        false,
+      ); // February never has a fifth Monday
+    });
+
+    test('returns true for last day of range', () {
+      final range =
+          DateTimeRange(start: DateTime(2023), end: DateTime(2023, 12, 31));
+      expect(
+        range.isNthDayOfMonth(5, DateTime.sunday, 12),
+        true,
+      ); // December has a Sunday on Dec.31
+    });
+  });
+
   group('DateTimeCheck extension', () {
     group('inRange', () {
       test('returns true when date is within range', () {
